13b5cc6d7b285abb423e03f2ce97449786495cbc
lucerlab <79510107+lucerlab@users.noreply.github.com>
2022-03-30 18:57:00 +0200

    Create __init__.py

---
+++ BootKMeans/__init__.py
@@ -0,0 +1,10 @@
+"""
+Bootstrapped KMeans using Python Sklearn
+
+This module contains an Sklearny class that combines Bootstrap resampling 
+with Sklearn implementation of K Means clustering to get a sense of cluster
+membership, centroids, and boundaries uncertainties.
+
+"""
+
+from BootstrapKMeans import KMeansBoot
